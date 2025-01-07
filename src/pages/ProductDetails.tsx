e4487e037c5e04e3d6053e074bcb4e00a8e5b9f1
Amohamed24 <mmohameda97@gmail.com>
2025-01-06 20:42:25 -0600

    Added the ProductDetails component with items details and action buttons

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,60 +1,78 @@
+'use client';
 import React, { useState } from "react";
+import Header from "../components/Header";
+import ProductCard from "../components/productCard"
 
-const StepCounter = () => {
-    const [counter, setCounter] = useState(0);
-    const [step, setStep] = useState(1);
 
-    const handleStepChange = (e) => {
-        // Update step based on user input
-        setStep(step - 1 || step + 1)
-    };
+interface ProductDetailsProps {
+    search: string;
+    setSearch: string;
+}
 
-    const increaseCount = () => {
-        // Increment counter by step
-        setCounter(counter + step)
-    };
+interface Products {
+    id: number;
+    title: string;     
+    category: string;  
+    gender: string;    
+    price: string;     
+    size: string;
+}
 
-    const decreaseCount = () => {
-        // Decrement counter by step
-        setCounter(counter - step)
-    };
 
-    const resetCount = () => {
-        setCounter(0)
-    };
-
+const ProductDetails = ({ search, setSearch, id, title, category, gender, price, size }) => {
+   
     return (
-        <div className="flex flex-col items-center justify-center p-5">
-            <h1>Counter: {counter}</h1>
+        <main>
+            <Header 
+                search={search}
+                setSearch={setSearch}
 
-            <input 
-                type="number" 
-                value={step} 
-                onChange={handleStepChange} 
-                className="border rounded p-2 my-3"
-                placeholder="Enter step value"
             />
 
-            <div className="flex space-x-4">
-                <button 
-                    onClick={decreaseCount} 
-                    className="border rounded p-3 bg-red-200 hover:bg-red-300">
-                    Decrease
-                </button>
-                <button 
-                    onClick={increaseCount} 
-                    className="border rounded p-3 bg-green-200 hover:bg-green-300">
-                    Increase
-                </button>
-            </div>
+            <div className="flex flex-row m-auto justify-center align-middle items-center h-[calc(100vh-80px)] gap-0 bg-gray-100">
+                <section className="w-full mx-5 flex justify-end">
+                    <div className="relative w-8/12 h-[35rem] overflow-hidden rounded-xl bg-teal-200">
+                        <img  
+                            alt={title} 
+                            className="w-full h-full object-cover" 
+                        />
+                    </div>
+                </section>
 
-            <button 
-                onClick={resetCount} 
-                className="border rounded p-3 bg-gray-200 hover:bg-gray-300 mt-4">
-                Reset
-            </button>
-        </div>
+                <section className="flex flex-col justify-between w-full mx-5 h-[35rem]">
+                    <div>
+                        <h1>Mens Clothes</h1>
+                        <h3 className="mb-3">Gender Category</h3>
+                        <h2>$40</h2>
+                    </div>
+
+                    <div className="my-10 w-7/12">
+                        <h2>Select Size</h2>
+                        <p className="mt-5 flex flex-wrap gap-5 [&>button]:border [&>button]:px-2 [&>button]:py-1 [&>button]:text-lg [&>button]:border-gray-400 
+                        [&>button]:rounded-[.25rem] [&>button]:cursor-pointer [&>button]:font-semibold [&>button]:w-[6rem] [&>button]:h-[3rem]">
+                            <button className="hover:border-black">XS</button>
+                            <button className="hover:border-black">S</button>
+                            <button className="hover:border-black">M</button>
+
+                            <button className="hover:border-black">L</button>
+                            <button className="hover:border-black">XL</button>
+                            <button className="hover:border-black">XXL</button>
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col gap-3 w-7/12 mt-0">
+                        <button className="border-none bg-teal-200 py-5 px-20 rounded-[2.5rem] font-semibold text-lg cursor-pointer hover:bg-teal-100">Add to Bag</button>
+                        <button className="border border-gray-400 py-5 px-20 rounded-[2.5rem] font-semibold text-lg cursor-pointer hover:border-gray-800">Favorite </button>
+                    </div>
+                </section>
+            </div>
+            
+           
+            
+
+            
+        </main>
     );
 };
 
-export default StepCounter;
+export default ProductDetails;
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,29 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
 import Products from "../data/Products";
 
 
 
+
 interface ProductListProps {
-  products: Product[];
+  id: number;
+  title: string;     
+  category: string;  
+  gender: string;    
+  price: string;     
+  size: string;
 }
 
-const ProductList = ({ products }: ProductListProps) => {
+
+const ProductList = ( ProductListProps: any) => {
+
   return (
     <div className="flex flex-wrap m-auto justify-center w-9/12 mt-2 gap-9">
       {Products.map((product) => (
-        <ProductCard
+       
+          <ProductCard
           key={product.id}
           title={product.title}
           category={product.category}
           gender={product.gender}
-          price={product.price}   
-        />
+          price={product.price} 
+          id={0} 
+          size={""}        
+          />        
       ))}
     </div>
   );
 };
 
 export default ProductList;
-
-
