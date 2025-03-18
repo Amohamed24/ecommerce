04c5b0c225d70f42094a7ecc347fb7a890c30292
Amohamed24 <mmohameda97@gmail.com>
2025-03-18 14:20:47 -0500

    Set up basic routes for user login and register

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+
+
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// Route for user register
+const registerUser = async (req, res) => {
+
+    res.json({ message: "Register API Working"})
+}
+
+
+
+
+export { loginUser, registerUser }
