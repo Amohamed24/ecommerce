d2763a4add71338e27f49d87d99b43abeee0c0f5
Amohamed24 <mmohameda97@gmail.com>
2025-03-27 11:19:32 -0500

    Fix cors issues for API requests

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,14 @@ app.use((req, res, next) => {
 // Standard CORS middleware as backup
 app.use(cors());
 
+// Explicit OPTIONS handler for all routes
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(200).end();
+});
+
 app.use(express.json());
 
 // API endpoints
